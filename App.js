f55c1fefa2dde06ee825a1f730edc4cdadaf0571
valencoronel <coronel.valentinet36@gmail.com>
2025-05-21 17:44:58 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,7 @@
+let respuesta = "";
+
+// Función que guarda la respuesta escrita por el usuario
+function guardarRespuesta() {
+    respuesta = document.getElementById("respuestaInput").value;
+    alert("Tu respuesta es: " + respuesta);
+}
